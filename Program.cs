a260e17c4e75ede1183f06a6d61bf101db402c4a
Marko <marko.saric@ferit.hr>
2022-10-27 17:25:34 +0200

    added index

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Luke Lešić!");
+            Console.WriteLine("Pozdrav iz aplikacije od Luke Lešić! 34565786544789");
         }
     }
 }
